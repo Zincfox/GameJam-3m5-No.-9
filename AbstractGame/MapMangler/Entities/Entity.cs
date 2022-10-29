b2b66a6a5036645a9297d4d29967bcd9528209d6
Alexander Schwerin <alexander_C._schwerin@web.de>
2022-10-29 12:02:46 +0200

    Add Entity stubs

---
+++ AbstractGame/MapMangler/Entities/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MapMangler.Rooms;
+
+namespace MapMangler.Entities
+{
+    public abstract class Entity
+    {
+        public Entity(int entityID)
+        {
+            this.entityID = entityID;
+        }
+
+        public readonly int entityID;
+
+        private Room? location = null;
+        public Room? Location {
+            get => location;
+            set
+            {
+                Room? oldLocation = location;
+                location = value;
+                oldLocation?.removeEntity(this);
+                value?.addEntity(this);
+                LocationChangeEvent?.Invoke(this, new LocationChangeEventArgs(this, oldLocation, value));
+            }
+        }
+
+        public event EventHandler<LocationChangeEventArgs>? LocationChangeEvent;
+
+        public class LocationChangeEventArgs : EventArgs
+        {
+            public readonly Room? from;
+            public readonly Room? to;
+            public readonly Entity entity;
+            public LocationChangeEventArgs(Entity entity, Room? from, Room? to)
+            {
+                this.from = from;
+                this.to = to;
+                this.entity = entity;
+            }
+        }
+    }
+}
--- AbstractGame/MapMangler/Rooms/Room.cs
+++ AbstractGame/MapMangler/Rooms/Room.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MapMangler.Entities;
 
 namespace MapMangler.Rooms
 {
@@ -10,5 +11,38 @@ namespace MapMangler.Rooms
 
         public IList<Room> Neighbours = new List<Room>();
 
+        private List<Entity> entities = new List<Entity>();
+
+        public IReadOnlyList<Entity> Entities { get => entities; }
+
+        internal void addEntity(Entity entity)
+        {
+            entities.Add(entity);
+            EntitiesChangeEvent?.Invoke(this, new RoomContentChangeEventArgs(this, entity, false));
+        }
+
+        internal void removeEntity(Entity entity)
+        {
+            if (!entities.Remove(entity)) return;
+            EntitiesChangeEvent?.Invoke(this, new RoomContentChangeEventArgs(this, entity, true));
+        }
+
+        public event EventHandler<RoomContentChangeEventArgs>? EntitiesChangeEvent;
+
+        public class RoomContentChangeEventArgs : EventArgs
+        {
+            public readonly Room room;
+            public readonly Entity entity;
+            private readonly bool isLeave;
+            public RoomContentChangeEventArgs(Room room, Entity entity, bool isLeave)
+            {
+                this.room = room;
+                this.entity = entity;
+                this.isLeave = isLeave;
+            }
+
+            public bool IsLeave() => isLeave;
+            public bool IsEnter() => !isLeave;
+        }
     }
 }
