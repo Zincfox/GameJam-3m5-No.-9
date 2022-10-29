2189acce65d74429aced2c9088bcf10598e949b2
Alexander Schwerin <alexander_C._schwerin@web.de>
2022-10-29 11:25:53 +0200

    Initialize AbstractGame project

---
+++ AbstractGame/MapMangler/Rooms/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapMangler.Rooms
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+
+        public IList<Room> Rooms = new List<Room>();
+    }
+}
