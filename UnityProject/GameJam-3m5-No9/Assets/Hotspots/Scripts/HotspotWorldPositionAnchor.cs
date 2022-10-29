dfa9153b2e012e21ffff4cd36ae1a37a93cbd65f
Lars Wobus <lars.wobus@web.de>
2022-10-29 13:41:38 +0200

    Fix runtime error when hotspot is in scene

--- UnityProject/GameJam-3m5-No9/Assets/Hotspots/Scripts/HotspotWorldPositionAnchor.cs
+++ UnityProject/GameJam-3m5-No9/Assets/Hotspots/Scripts/HotspotWorldPositionAnchor.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using UnityEditor;
 #endif
 
-[ExecuteInEditMode]
+[ExecuteAlways]
 public class HotspotWorldPositionAnchor : MonoBehaviour
 {
     [SerializeField]
@@ -15,13 +15,20 @@ public class HotspotWorldPositionAnchor : MonoBehaviour
     [SerializeField]
     private MeshRenderer dummyMeshRenderer;
 
+#if UNITY_EDITOR
     private void Awake()
     {
         if (uiHotspot == null)
         {
-            SetupUiHotspotElement();
+            InitCanvasElement();
+        }
+
+        if (dummyMeshRenderer == null)
+        {
+            InitRemainingFields();
         }
     }
+#endif
 
     private void Start()
     {
@@ -34,11 +41,10 @@ public class HotspotWorldPositionAnchor : MonoBehaviour
         {
             transform.hasChanged = false;
             uiHotspot.transform.position = Camera.main.WorldToScreenPoint(transform.position);
-            Debug.Log(uiHotspot.transform.position);
         }
     }
 
-    private void SetupUiHotspotElement ()
+    private void InitCanvasElement ()
     {
         var hotspotGroup = GameObject.Find("Hotspot CanvasGroup"); // TODO
         Assert.IsNotNull(hotspotGroup, "Hotspotgroup not found");
@@ -50,24 +56,27 @@ public class HotspotWorldPositionAnchor : MonoBehaviour
 
         var uiElement = Instantiate(prefab);
         uiElement.transform.SetParent(hotspotGroup.transform);
-        StartCoroutine(UpdateName(uiElement, prefab));
+        uiElement.transform.localScale = Vector3.one;
+        StartCoroutine(ChangeCanvasElementNameInNextFrame(uiElement, prefab));
         
-
         uiHotspot = uiElement.GetComponent<HotspotBehaviour>();
         Assert.IsNotNull(uiHotspot, $"Could not find {nameof(HotspotBehaviour)} on {uiElement.name}");
         uiHotspot.SetOwner(this);
+    }
 
+    private IEnumerator ChangeCanvasElementNameInNextFrame(GameObject target, GameObject prefab)
+    {
+        yield return null;
+        target.name = $"{prefab.name} of {gameObject.name}";
+    }
+
+    private void InitRemainingFields()
+    {
         var childName = "HotspotAnchor Visual Helper";
         var helper = gameObject.FindChildByName(childName);
         Assert.IsNotNull(helper, $"Could not find child {childName}");
 
         dummyMeshRenderer = helper.GetComponent<MeshRenderer>();
         Assert.IsNotNull(dummyMeshRenderer, $"Could not find MeshRenderer component on {dummyMeshRenderer.name}");
-    }
-
-    private IEnumerator UpdateName(GameObject target, GameObject prefab)
-    {
-        yield return null;
-        target.name = $"{prefab.name} of {gameObject.name}";
     }
 }
